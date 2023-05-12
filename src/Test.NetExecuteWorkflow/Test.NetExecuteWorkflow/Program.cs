49362504149f1b881c46bb1d881c3ade4c7b7e7f
Thindal <thindaltv@gmail.com>
2023-05-12 19:26:36 +0200

    First add with basic application

---
+++ src/Test.NetExecuteWorkflow/Test.NetExecuteWorkflow/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start!");
+
+if(!Directory.Exists("out"))
+{
+    Directory.CreateDirectory("out");
+}
+
+string result = """{ "output": "value"}""";
+File.WriteAllText("out/result.json", result);
+Console.WriteLine("Stop");
